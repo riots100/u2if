bd39c787798d2e4734290ad2aafabd536fa31253
Bruce Johnson <johnson@alaretech.com>
2024-08-13 14:23:57 -0700

    update default build settings

--- firmware/build-all.bat
+++ firmware/build-all.bat
@@ -11,15 +11,15 @@ if /i not "%dirname%"=="firmware" (
 )
 
 REM Get the version from CMakeLists.txt
-for /f "tokens=2 delims== " %%a in ('findstr "project(u2if VERSION" "%CD%\source\CMakeLists.txt"') do set "VERSION=%%a"
+rem for /f "tokens=2 delims== " %%a in ('findstr "project(u2if VERSION" "%CD%\source\CMakeLists.txt"') do set "VERSION=%%a"
 
 set "RELEASE_DIR=%CD%\release"
 
 REM Get the number of processors
-for /f %%a in ('wmic cpu get NumberOfLogicalProcessors /value') do (
-    for /f "delims=" %%b in ("%%a") do set "CORES_NB=%%b"
-)
-set "CORES_NB=%CORES_NB:NumberOfLogicalProcessors=%%"
+rem for /f %%a in ('wmic cpu get NumberOfLogicalProcessors /value') do (
+rem     for /f "delims=" %%b in ("%%a") do set "CORES_NB=%%b"
+rem )
+rem set "CORES_NB=%CORES_NB:NumberOfLogicalProcessors=%%"
 
 REM Function to build the project
 :build
@@ -29,8 +29,8 @@ REM Function to build the project
     set "FIRMWARE_ROOT_DIR=%CD%"
     cd /d "%tmp_dir%"
     cmake -G Ninja -DBOARD=%2 -DI2S_ALLOW=%3 -DWS2812_ENABLED=%4 -DWS2812_SIZE=%5 -DHUB75_ALLOW=%6 "%FIRMWARE_ROOT_DIR%\source"
-    ninja -j%CORES_NB%
-    copy u2if.uf2 "%RELEASE_DIR%\u2if_%1_v%VERSION%.uf2"
+    ninja 
+    copy u2if.uf2 "%RELEASE_DIR%\u2if_%1.uf2"
     cd /d "%FIRMWARE_ROOT_DIR%"
     rmdir /s /q "%tmp_dir%"
 goto :eof
